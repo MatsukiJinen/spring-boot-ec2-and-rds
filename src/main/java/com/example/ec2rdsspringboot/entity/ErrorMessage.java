daab78b04842161611e5a5d498b5c2b5f14f4b59
Matsuki Jinen <matsukijinen@matsugishizens-MacBook-Air.local>
2022-11-24 16:14:50 +0900

    add ErrorMessage entity

--- src/main/java/com/example/ec2rdsspringboot/entity/ErrorMessage.java
+++ src/main/java/com/example/ec2rdsspringboot/entity/ErrorMessage.java
@@ -1,2 +1,14 @@
-package com.example.ec2rdsspringboot.entity;public class ErrorMessage {
+package com.example.ec2rdsspringboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private HttpStatus status;
+    private String message;
 }
